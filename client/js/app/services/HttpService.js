a1c66a81c7c9ecc3bc1e25682e7dfe0b8c3c4fa4
Déia <Dia>
2017-01-26 00:48:09 -0200

    JS III avançado aula 06

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,40 +1,25 @@
 class HttpService{
 
+    _handleErrors(res){
+        if(!res.ok) throw new Error(res.statusText);
+        return res;
+    }
+
     get(url){
 
-        return new Promise( (resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
-                        resolve( JSON.parse(xhr.responseText) );
-                    }else{
-                        reject(xhr.responseText);
-                    }
-                }
-            }
-            xhr.send();
-        })
-
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
     }
 
     post(url, dados){
-        return new Promise( (resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('POST', url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
-                        resolve( JSON.parse(xhr.responseText) );
-                    }else{
-                        reject(xhr.responseText);
-                    }
-                }
-            }
-            xhr.send(JSON.stringify(dados));
+
+        return fetch(url, {
+            headers: {'Content-type' : 'application/json'},
+            method: 'post',
+            body: JSON.stringify(dados)
         })
+        .then(res => this._handleErrors(res));
     }
 
 }
